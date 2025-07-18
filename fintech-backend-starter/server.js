be514a062e049e60084bc9f97cda17af8e3f453e
BEAVER4RIGHTS <knights4rights.uk@gmail.com>
2025-07-18 02:33:25 +0100

    Update server.js

--- fintech-backend-starter/server.js
+++ fintech-backend-starter/server.js
@@ -18,3 +18,7 @@ app.use('/api/wallet', walletRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.get('/', (req, res) => {
+  res.send('Fintech backend is live!');
+});
